9d3e5942a5073977327d3d3f70747ecd384c7e72
Shivansh Chandnani <schandnani@outlook.com>
2022-08-21 09:39:25 -0700

    Print poetry versio

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
-curl -sSL https://install.python-poetry.org | python3 - --version 1.2.0b2
+poetry --version
 poetry install
 
 python manage.py collectstatic --no-input
