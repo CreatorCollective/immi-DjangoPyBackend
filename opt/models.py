eafce1a19593d70aa91726c656aad8d6b336788e
Shivansh Chandnani <schandnani@outlook.com>
2022-08-27 17:01:16 -0700

    add views

--- opt/models.py
+++ opt/models.py
@@ -5,3 +5,7 @@ class Person(models.Model):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     email = models.CharField(max_length=256, null=True, blank=True)
+
+class Counters(models.Model):
+    counter_name = models.CharField(max_length=256)
+    total_views = models.BigIntegerField()
